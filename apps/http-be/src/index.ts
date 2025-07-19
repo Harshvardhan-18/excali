ea872f3dffb7201afc6431d851962cdb22c68332
Harshvardhan <harshvar8an@gmail.com>
2025-07-19 09:51:09 +0530

    added zod

--- apps/http-be/src/index.ts
+++ apps/http-be/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/be-common/config";
 import { middleware } from "./middleware";
+import {createRoomSchema, createUserSchema, signInSchema} from "@repo/common/types";
 
 
 const app=express();
@@ -9,6 +10,12 @@ const app=express();
 app.use(express.json());
 
 app.post("/signin",(req,res)=>{
+    const data=signInSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"Incorrect inputs"
+        })
+    }
     const userId=1;
     const token=jwt.sign({userId},JWT_SECRET);
 
@@ -17,11 +24,23 @@ app.post("/signin",(req,res)=>{
     })
 })
 app.post("/signup",(req,res)=>{
+    const data=createUserSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"Incorrect inputs"
+        })
+    }
     res.json({
         userId:"123"
     })
 })
 app.post("/room",middleware,(req,res)=>{
+    const data=createRoomSchema.safeParse(req.body);
+    if(!data.success){
+        return res.json({
+            message:"Incorrect inputs"
+        })
+    }
     res.json({
         roomId:123
     })
